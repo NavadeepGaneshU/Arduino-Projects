faf7a195a82252b7f462bbcc5538338d82c2cbf7
Navadeep Ganesh U <52508011+NavadeepGaneshU@users.noreply.github.com>
2020-07-28 21:03:17 +0530

    Update Blynk_DHT11.ino

--- Blynk_DHT11.ino
+++ Blynk_DHT11.ino
@@ -1,34 +1,9 @@
-//Viral science www.youtube.com/c/viralscience
-//Temperature sensor
-/**************************************************************
- * Blynk is a platform with iOS and Android apps to control
- * Arduino, Raspberry Pi and the likes over the Internet.
- * You can easily build graphic interfaces for all your
- * projects by simply dragging and dropping widgets.
- *
- *   Downloads, docs, tutorials: http://www.blynk.cc
- *   Blynk community:            http://community.blynk.cc
- *   Social networks:            http://www.fb.com/blynkapp
- *                               http://twitter.com/blynk_app
- *
- * Blynk library is licensed under MIT license
- * This example code is in public domain.
- *
- **************************************************************
- * This example shows how value can be pushed from Arduino to
- * the Blynk App.
- *
- * WARNING :
- * For this example you'll need SimpleTimer library:
- *   https://github.com/jfturcot/SimpleTimer
- * and Adafruit DHT sensor library:
- *   https://github.com/adafruit/DHT-sensor-library
- *
- * App project setup:
- *   Value Display widget attached to V5
- *   Value Display widget attached to V6
- *
- **************************************************************/
+  
+/*Sending Digital Temperature and Humidity(DHT11) sensor's data to Mathworks Thingspeak!
+https://www.clevertronics.blogspot.com
+Used Hardware : ESP8266 (NodeMCU)
+Used IDE : Arduino IDE 1.8.12
+*/
 
 #define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <SPI.h>
