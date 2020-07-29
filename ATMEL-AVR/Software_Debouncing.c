a1e2367ae90537edc963d100ce99295f44c1fcf6
Navadeep Ganesh U <52508011+NavadeepGaneshU@users.noreply.github.com>
2020-07-29 21:35:12 +0530

    Update and rename Software_Debounce.c to Software_Debouncing.c
    code updated

--- ATMEL-AVR/Software_Debouncing.c
+++ ATMEL-AVR/Software_Debouncing.c
@@ -1,12 +1,22 @@
-//SWITCH - Software Debouncing!
+/*SWITCH - Software Debouncing!
+Debouncing is an effect where the trigger interrupt or simply a switch toggle bounces back.
+This usually happens when microcontroller considers one click as more than one.
+Due to this, a kind of interference occurs which makes multiple attenuated toggling just with one being physically done.
+
+Here is a novel method to prevent this effect with confidence level technique. 
+PORTC is chosen here with
+PINC0 --> LED1,
+PINC1 --> SWITCH,
+PINC2 --> LED2.
+*/
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
 int main(void)
-{
-	DDRC |= 1 << PINC0;
+{       
+	DDRC |= 1 << PINC0; 
 	PORTC ^=1 << PINC0;
 	DDRC |= 1<< PINC2;
 	DDRC &= ~(1 <<PINC1);
@@ -18,11 +28,10 @@ int main(void)
 
 	while (1)
 	{
-
 		if (bit_is_clear(PINC,1))
 		{
 			pressed_confidence_level ++;
-			if(pressed_confidence_level >500)
+			if(pressed_confidence_level >500) // pressed_confidence_level > "500" is subjected to change based on trial and errors.
 			{
 				if (pressed==0)
 				{
@@ -41,7 +50,6 @@ int main(void)
 				pressed = 0;
 				released_confidence_level = 0;
 			}
-			
 		}
 	}
 }
