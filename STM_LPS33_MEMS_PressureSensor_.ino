bef2717d9af8bd5c9a5392f3571ee4bc9e406d31
Navadeep Ganesh U <52508011+NavadeepGaneshU@users.noreply.github.com>
2021-01-03 00:15:33 +0530

    Update STM_LPS33_MEMS_PressureSensor_.ino
    filtered

--- STM_LPS33_MEMS_PressureSensor_.ino
+++ STM_LPS33_MEMS_PressureSensor_.ino
@@ -1,11 +1,11 @@
 /*
-STMicroelectronics LPS33W MEMS Pressure Sensor interphase with Arduino UNO!
+STMicroelectronics LPS33W MEMS Pressure Sensor interface with Arduino UNO!
 https://www.clevertronics.blogspot.com
 Used Hardware : Arduino UNO
 Used IDE : Arduino IDE 1.8.12
 
 Connection can be done through SPI or I2C. Below, SPI part is commented and I2C is used. 
-For SPI Interphase:      For I2C Interphase:
+For SPI Interphase:      For I2C interface:
 MI--> PIN 12;            SDA - PIN A4
 MO--> PIN 11;            SCL - PIN A5
 CLK--> PIN 13;
@@ -21,22 +21,24 @@ Adafruit_LPS35HW lps35hw = Adafruit_LPS35HW();
 //#define LPS_MISO 12
 //#define LPS_MOSI 11
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
  
-  if (!lps35hw.begin_I2C())  //For I2C Interphase
+  if (!lps35hw.begin_I2C())  //For I2C interface
   { 
-  //if (!lps35hw.begin_SPI(LPS_CS))  //For SPI Interphase
-    { 
+  //if (!lps35hw.begin_SPI(LPS_CS))  //For SPI interface
+  //  { 
   //if (!lps35hw.begin_SPI(LPS_CS, LPS_SCK, LPS_MISO, LPS_MOSI)) 
-      {
+  //  {
     Serial.println("Couldn't find LPS35HW chip");
     while (1);
   }
   Serial.println("Found LPS35HW chip");
 }
  
-void loop() {
+void loop() 
+{
   Serial.print("Temperature: ");
   Serial.print(lps35hw.readTemperature());
   Serial.println(" C");
